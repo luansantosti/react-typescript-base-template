b7dfc9ce7514e595e7f4a17150b3746eafc76fe6
luansantosti <luanjr21@gmail.com>
2021-12-09 18:07:29 -0300

    webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,61 +1,46 @@
-import path from 'path';
-import { Configuration, DefinePlugin } from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const webpackConfig = (): Configuration => ({
-  entry: './src/index.tsx',
-  ...(process.env.production || !process.env.development
-    ? {}
-    : { devtool: 'eval-source-map' }),
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
-  },
-  output: {
-    path: path.join(__dirname, '/build'),
-    filename: 'build.js',
-  },
-  module: {
-    rules: [
-      {
+module.exports = {
+	entry: {
+		app: path.resolve(__dirname, './src/index.tsx'),
+		vendor: ["styled-components"],
+	},
+ 	output: {
+   	path: path.join(__dirname, '/dist'),
+   	filename: '[name].js',
+		publicPath: '/'
+ 	},
+	module: {
+		rules: [
+			{
         test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
-        },
-        exclude: /build/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  devServer: {
-    port: 3333,
-    open: true,
+		]
+	},
+	devServer: {
     historyApiFallback: true,
+		port: 3000,
+		hot: true
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
-      template: './public/index.html',
-    }),
-    // DefinePlugin allows you to create global constants which can be configured at compile time
-    new DefinePlugin({
-      'process.env': process.env.production || !process.env.development,
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      // Speeds up TypeScript type checking and ESLint linting
-      // (by moving each to a separate process)
-      eslint: {
-        files: './src/**/*.{ts,tsx,js,jsx}',
-      },
-    }),
-  ],
-});
-
-export default webpackConfig;
+	resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+	optimization: {
+		runtimeChunk: "single",
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					name: 'vendor',
+          filename: 'vendor.js',
+          test: 'vendor',
+          chunks: 'initial',
+          enforce: true,
+				}
+			}
+		}
+	},
+ 	plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+}
